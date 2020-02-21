c1b22deefff96f4d3b0a96acc6710d71b7a6d11a
malinik16 <59576206+malinik16@users.noreply.github.com>
2020-02-21 10:18:56 +0530

    Add files via upload

---
+++ fib.java
@@ -0,0 +1,15 @@
+class mod
+{
+public static void main(String args[])
+{
+int a=563;
+z=a%10;
+int b =a/10;
+y=b%10;
+x=a/10;
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+}
+}
+
